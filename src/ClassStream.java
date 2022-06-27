c2bbd72775bc2f39a83e323ecc4b2aa21815aa31
AJK\Shcherbet <fox2703@gmail.com>
2022-06-27 14:19:03 +0300

    Finished

--- src/ClassStream.java
+++ src/ClassStream.java
@@ -17,14 +17,14 @@ public class ClassStream {
         String[] masString_1 = {"raz", "dva", "tri", "chetire", "pyat", "shest"};
         String[] masString_2 = {"1", "5", "8", "10"};
 
-//        System.out.println("odded(list) = " + classStream.odded(list));
-//        System.out.println("upperCase(list) = " + classStream.upperCase(list));
-//        System.out.println("classStream.getDigits(masString_2) = " + classStream.getDigits(masString_2));
+        System.out.println("odded(list) = " + classStream.odded(list));
+        System.out.println("upperCase(list) = " + classStream.upperCase(list));
+        System.out.println("classStream.getDigits(masString_2) = " + classStream.getDigits(masString_2));
+////        System.out.println("zip(Arrays.stream(masString_1), Arrays.stream(masInteger_1)) = " +
+                zip(Arrays.stream(masString_2), Arrays.stream(masString_1)).forEach(e -> System.out.println(e));
 ////        System.out.println("classStream.getGenerator(25214903917L, 11L,  2^48L, 1L) = " +
 //          classStream.getGenerator(25214903917L, 11L, 2 ^ 48L, 1L)
 //                  .forEach(System.out::println);
-////        System.out.println("zip(Arrays.stream(masString_1), Arrays.stream(masInteger_1)) = " +
-//                zip(Arrays.stream(masString_2), Arrays.stream(masString_1)).forEach(e -> System.out.println(e));
     }
 
     public String odded(List<String> list) {
